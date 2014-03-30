5d26ca8b4a6e3671a799780e5eec789e89a51efd
Alexander Stillesjö <a.stillesjo@gmail.com>
2014-03-30 21:27:10 +0200

    Improved setup script

--- setup.sh
+++ setup.sh
@@ -7,23 +7,30 @@ function backup_files()
 {
   FILE="$HOME/.vimrc"
   FOLDER="$HOME/.vim"
-  if [[ ! -L "$FILE" ]]
-  then
+  if [[ ! -L "$FILE" ]]; then
     mv ~/.vimrc ~/.vimrc_backup
   fi
-  if [[ ! -L "$FOLDER" ]]
-  then
-    mv $FOLDER "${FOLDER}.backup"
+  if [[ ! -L "$FOLDER" ]]; then
+    # if the current directory is ~/.vim we shouldn't move it.
+    if [[ $FOLDER == `pwd` ]]; then
+      echo "$FOLDER is the same as `pwd`. Won't move the folder."
+    else
+      mv $FOLDER "${FOLDER}.backup"
+    fi
   fi
 }
 
 function install_files()
 {
   path=`pwd`
-  # Install vimrc file in home folder
+  FOLDER="$HOME/.vim"
+  # if the current directory isn't ~/.vim we make a symlink.
+  if [[ ! $FOLDER == $path ]]; then
+    cd
+    ln -s $path .vim
+  fi
   cd
   ln -s $path/vimrc .vimrc
-  ln -s $path .vim
 }
 
 
