4e2d4bdf99da9f2088ff67987b8a2b151be0282e
Alexander Stillesjö <a.stillesjo@gmail.com>
2015-03-22 17:13:52 +0100

    Stuff

--- setup.sh
+++ setup.sh
@@ -14,14 +14,17 @@ mkdir $HOME/.vim/colors
 
 # Install pathogen
 curl -LSso $HOME/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# Install colorschemes
 curl -LSso $HOME/.vim/colors/codeschool.vim https://raw.githubusercontent.com/flazz/vim-colorschemes/master/colors/codeschool.vim
+curl -LSso $HOME/.vim/colors/solarized.vim https://github.com/altercation/vim-colors-solarized/blob/master/colors/solarized.vim
 
+# Clone Vundle, bundle manager
 git clone https://github.com/gmarik/Vundle.vim $HOME/.vim/bundle/Vundle.vim
-
 
 if [ -a $HOME/.vimrc ]
 then
-  echo "Cause I'm backing up backing up..."
+  echo "backing up..."
   mv $HOME/.vimrc $HOME/.vimrcbackup
 fi
 curl -LSso $HOME/.vimrc https://raw.githubusercontent.com/stillesjo/vimrc/master/.vimrc
