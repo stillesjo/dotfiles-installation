96bd9a990da9463daaaab634d632870bdc50dac5
Alexander Stillesjö <a.stillesjo@gmail.com>
2015-03-22 16:32:33 +0100

    Made it useable ;)

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ -d $HOME/.vim ]
+then
+  echo "Folder exists... Will backup";
+  mv $HOME/.vim $HOME/.vimbackup
+else
+  echo "Folder didn't exists since before"
+fi
+
+mkdir -p $HOME/.vim/autoload
+mkdir  $HOME/.vim/bundle
+mkdir $HOME/.vim/colors
+
+# Install pathogen
+curl -LSso $HOME/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+curl -LSso $HOME/.vim/colors/codeschool.vim https://raw.githubusercontent.com/flazz/vim-colorschemes/master/colors/codeschool.vim
+
+git clone https://github.com/gmarik/Vundle.vim $HOME/.vim/bundle/Vundle.vim
+
+
+if [ -a $HOME/.vimrc ]
+then
+  echo "Cause I'm backing up backing up..."
+  mv $HOME/.vimrc $HOME/.vimrcbackup
+fi
+cp `dirname $0`/.vimrc $HOME
+
+# Run vim and install all the bundles.
+vim +PluginInstall +qall
